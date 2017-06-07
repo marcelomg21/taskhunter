db7fe81d880fc38c1f7812e4320bd371fdf07469
marcelomg21 <marcelomg21@gmail.com>
2017-06-07 15:55:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ var initDb = function(callback) {
     dbDetails.databaseName = db.databaseName;
     dbDetails.url = mongoURLLabel;
     dbDetails.type = 'MongoDB';
-
+    db.collection('positions').ensureIndex( { location : "2dsphere" } );
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
