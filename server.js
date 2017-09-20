e8617d1d70ddb498b42b3195984262107a03dbab
marcelomg21 <marcelomg21@gmail.com>
2017-09-20 17:11:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -230,7 +230,7 @@ app.get('/api/conversations/:conversation_id/messages/', function (req, res) {
           data: [{
               id: 1,
               message: 'Boa noite, gostaria de um orçamento para a troca do Chuveiro?',
-              creation_date: '2013-01-17T08:05:00.124-03:00',
+              creation_date: '2017-01-17',
               sender: { 
                   id: 102,                  
                   first_name: 'Moacir',
@@ -249,7 +249,7 @@ app.get('/api/conversations/:conversation_id/messages/', function (req, res) {
           {
               id: 2,
               message: 'Isso é um teste do chuveiro..',
-              creation_date: '2013-01-17T08:06:00.124-03:00',
+              creation_date: '2017-01-17',
               sender: { 
                   id: 102,                  
                   first_name: 'Moacir',
@@ -268,10 +268,29 @@ app.get('/api/conversations/:conversation_id/messages/', function (req, res) {
           {
               id: 3,
               message: 'Sim, são 50.00',
-              creation_date: '2013-01-17T08:07:00.124-03:00',
+              creation_date: '2017-01-17',
               sender: { 
                   id: 1520675761317155,                  
                   first_name: 'Marcelo',
+                  age: 30,
+                  profiles: [{
+                      id: 102,
+                      mode: 0,
+                      
+                      width: 50,
+                      height: 50
+                  }]
+              },
+              clickable_profile_link: false,
+              clickable_message_link: false
+          },
+          {
+              id: 4,
+              message: 'Certo, obrigado',
+              creation_date: '2017-01-17',
+              sender: { 
+                  id: 102,                  
+                  first_name: 'Moacir',
                   age: 30,
                   profiles: [{
                       id: 102,
