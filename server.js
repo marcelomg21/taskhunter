dc64c8f80ee307610b0a7b5ceae8d2d52475cfb7
marcelomg21 <marcelomg21@gmail.com>
2017-09-28 19:31:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express'),
     jwt     = require('jsonwebtoken'),
     assert = require('assert'),
     request = require('request-promise'),
+    firebase = require('firebase-admin'),
     bodyParser = require('body-parser');
     
 Object.assign=require('object-assign');
@@ -62,6 +63,63 @@ var initDb = function(callback) {
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
+
+//firebase FCM
+var API_KEY = "AIzaSyDZyILex2S1s6UpHyHG6d7HYON7hxOQ4g0"; // Your Firebase Cloud Messaging Server API key
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+// Initialize the app with a service account, granting admin privileges
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://taskhunterapp.firebaseio.com"
+});
+ref = firebase.database().ref();
+
+function listenForNotificationRequests() {
+  var requests = ref.child('notificationRequests');
+  requests.on('child_added', function(requestSnapshot) {
+    var request = requestSnapshot.val();
+    sendNotificationToUser(
+      request.username, 
+      request.message,
+      function() {
+        requestSnapshot.ref.remove();
+      }
+    );
+  }, function(error) {
+    console.error(error);
+  });
+};
+
+function sendNotificationToUser(username, message, onSuccess) {
+  request({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type' :' application/json',
+      'Authorization': 'key='+API_KEY
+    },
+    body: JSON.stringify({
+      notification: {
+        title: message
+      },
+      to : '/topics/user_'+username
+    })
+  }, function(error, response, body) {
+    if (error) { console.error(error); }
+    else if (response.statusCode >= 400) { 
+      console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+    }
+    else {
+      onSuccess();
+    }
+  });
+}
+
+// start listening
+listenForNotificationRequests();
 
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
