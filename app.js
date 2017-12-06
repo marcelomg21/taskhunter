79bab83b31157cc6406215699b0d3f59f48b14b6
marcelomg21 <marcelomg21@gmail.com>
2017-12-06 18:15:02 -0200

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// Database
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest2');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -28,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
-    req.db = db;
+    //req.db = db;
     next();
 });
 
