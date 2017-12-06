19d94c103ae74c30ec8b9242afe8a3c590d848c3
marcelomg21 <marcelomg21@gmail.com>
2017-12-06 18:22:14 -0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,11 @@ var router = express.Router();
  * GET userlist.
  */
 router.get('/userlist', function(req, res) {
-    /*var db = req.db;
-    var collection = db.get('users');
+    var db = req.db;
+    var collection = db.get('userlist');
     collection.find({},{},function(e,docs){
         res.json(docs);
-    });*/
-    res.render('index', { title: 'MARCELO' });
+    });
 });
 
 /*
@@ -18,7 +17,7 @@ router.get('/userlist', function(req, res) {
  */
 router.post('/adduser', function(req, res) {
     var db = req.db;
-    var collection = db.get('users');
+    var collection = db.get('userlist');
     collection.insert(req.body, function(err, result){
         res.send(
             (err === null) ? { msg: '' } : { msg: err }
@@ -31,7 +30,7 @@ router.post('/adduser', function(req, res) {
  */
 router.delete('/deleteuser/:id', function(req, res) {
     var db = req.db;
-    var collection = db.get('users');
+    var collection = db.get('userlist');
     var userToDelete = req.params.id;
     collection.remove({ '_id' : userToDelete }, function(err) {
         res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
