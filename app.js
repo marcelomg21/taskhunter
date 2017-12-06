dcaaed408dba03de7c46fed6da5671855e59b54e
marcelomg21 <marcelomg21@gmail.com>
2017-12-06 17:41:17 -0200

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 // Database
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest2');
+//var mongo = require('mongodb');
+//var monk = require('monk');
+//var db = monk('localhost:27017/nodetest2');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +20,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
-    req.db = db;
+    //req.db = db;
     next();
 });
 
