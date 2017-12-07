23be08042c34233fc9b8f3533282314ce582264f
marcelomg21 <marcelomg21@gmail.com>
2017-12-07 17:15:13 -0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,18 @@ var router = express.Router();
  * GET userlist.
  */
 router.get('/userlist', function(req, res) {
-    /*var db = req.db;
-    var collection = db.collection('userlist');
-    collection.find({},{},function(e,docs){
-        res.json(docs);
-    });*/
-    
     var db = req.db;
-    var query = {};    
-
-    db.collection('userlist').find(query).toArray(function (err, docs) {                                   
-            return res.json(docs);                
+    var collection = db.collection('userlist');
+    collection.find({},function(e,docs){
+        res.json(docs);
     });
+    
+    //var db = req.db;
+    //var query = {};    
+
+    /*db.collection('userlist').find(query).toArray(function (err, docs) {                                   
+            return res.json(docs);                
+    });*/
 });
 
 /*
