d413f3ad06f09bed3b58f3516b6aaa28d93bc6f0
marcelomg21 <marcelomg21@gmail.com>
2017-12-11 00:27:51 -0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,11 @@
 var express = require('express');
+var router = express.Router();
 
-module.exports = (function () {
-    'use strict';
-    var router = express.Router();
-    
-    router.get('/home', function (req, res) {
-        res.render('carousel', {
-            'pathToAssets': '/bootstrap-3.3.1',
-            'pathToSelectedTemplateWithinBootstrap' : '/bootstrap-3.3.1/docs/examples/carousel'
-        });
+router.get('/home', function(req, res) {
+    res.render('carousel', {
+        'pathToAssets': '/bootstrap-3.3.1',
+        'pathToSelectedTemplateWithinBootstrap' : '/bootstrap-3.3.1/docs/examples/carousel'
     });
-    return router;
-})();
+});
+
+module.exports = router;
