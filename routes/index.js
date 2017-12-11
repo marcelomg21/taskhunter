3826f02951951f7a133f33bd6e63fe0ae04ea3a2
marcelomg21 <marcelomg21@gmail.com>
2017-12-11 12:07:50 -0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
+/*router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});*/
+
+router.get('/', function(req, res) {
+    res.render('carousel', {
+        'pathToAssets': '/bootstrap-3.3.1',
+        'pathToSelectedTemplateWithinBootstrap' : '/bootstrap-3.3.1/docs/examples/carousel'
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('signin', {
+        'pathToAssets': '/bootstrap-3.3.1',
+        'pathToSelectedTemplateWithinBootstrap' : '/bootstrap-3.3.1/docs/examples/signin'
+    });
 });
 
 module.exports = router;
