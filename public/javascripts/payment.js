2c510992cb0920292570c82f7a1062492609862f
marcelomg21 <marcelomg21@gmail.com>
2017-12-29 11:12:11 -0200

    Update payment.js

--- public/javascripts/payment.js
+++ public/javascripts/payment.js
@@ -1,5 +1,5 @@
 // Userlist data array for filling in info box
-var userListData = [];
+var paymentListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
@@ -8,13 +8,13 @@ $(document).ready(function() {
     populateTable();
 
     // Username link click
-    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+    $('#paymentList table tbody').on('click', 'td a.linkshowuser', showPaymentInfo);
 
     // Add User button click
-    $('#btnAddUser').on('click', addUser);
+    //$('#btnAddUser').on('click', addUser);
 
     // Delete User link click
-    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+    //$('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 
 });
 
@@ -27,22 +27,31 @@ function populateTable() {
     var tableContent = '';
 
     // jQuery AJAX call for JSON
-    $.getJSON( '/users/userlist', function( data ) {
+    $.getJSON( '/payment/paymentlist', function( data ) {
 
         // Stick our user data array into a userlist variable in the global object
-        userListData = data;
+        paymentListData = data;
 
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this._id + '" title="Show Details">' + this._id + '</a></td>';
+            tableContent += '<td>' + this.matching + '</td>';
+            tableContent += '<td>' + this.working + '</td>';
+            tableContent += '<td>' + this.name + '</td>';
+            tableContent += '<td>' + this.type + '</td>';
+            tableContent += '<td>' + this.card + '</td>';
+            tableContent += '<td>' + this.condition + '</td>';
+            tableContent += '<td>' + this.date + '</td>';
+            tableContent += '<td>' + this.price + '</td>';
+            tableContent += '<td>' + this.tax + '</td>';
+            tableContent += '<td>' + this.paid + '</td>';
+            //tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
         });
 
         // Inject the whole content string into our existing HTML table
-        $('#userList table tbody').html(tableContent);
+        $('#paymentList table tbody').html(tableContent);
     });
 };
 
@@ -53,113 +62,25 @@ function showUserInfo(event) {
     event.preventDefault();
 
     // Retrieve username from link rel attribute
-    var thisUserName = $(this).attr('rel');
+    var thisID = $(this).attr('rel');
 
     // Get Index of object based on id value
-    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem._id; }).indexOf(thisID);
 
     // Get our User Object
-    var thisUserObject = userListData[arrayPosition];
+    var thisPaymentObject = userListData[arrayPosition];
 
     //Populate Info Box
-    $('#userInfoName').text(thisUserObject.fullname);
-    $('#userInfoAge').text(thisUserObject.age);
-    $('#userInfoGender').text(thisUserObject.gender);
-    $('#userInfoLocation').text(thisUserObject.location);
-
-};
-
-// Add User
-function addUser(event) {
-    event.preventDefault();
-
-    // Super basic validation - increase errorCount variable if any fields are blank
-    var errorCount = 0;
-    $('#addUser input').each(function(index, val) {
-        if($(this).val() === '') { errorCount++; }
-    });
-
-    // Check and make sure errorCount's still at zero
-    if(errorCount === 0) {
-
-        // If it is, compile all user info into one object
-        var newUser = {
-            'username': $('#addUser fieldset input#inputUserName').val(),
-            'email': $('#addUser fieldset input#inputUserEmail').val(),
-            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
-            'age': $('#addUser fieldset input#inputUserAge').val(),
-            'location': $('#addUser fieldset input#inputUserLocation').val(),
-            'gender': $('#addUser fieldset input#inputUserGender').val()
-        }
-
-        // Use AJAX to post the object to our adduser service
-        $.ajax({
-            type: 'POST',
-            data: newUser,
-            url: '/users/adduser',
-            dataType: 'JSON'
-        }).done(function( response ) {
-
-            // Check for successful (blank) response
-            if (response.msg === '') {
-
-                // Clear the form inputs
-                $('#addUser fieldset input').val('');
-
-                // Update the table
-                populateTable();
-
-            }
-            else {
-
-                // If something goes wrong, alert the error message that our service returned
-                alert('Error: ' + response.msg);
-
-            }
-        });
-    }
-    else {
-        // If errorCount is more than 0, error out
-        alert('Please fill in all fields');
-        return false;
-    }
-};
-
-// Delete User
-function deleteUser(event) {
-
-    event.preventDefault();
-
-    // Pop up a confirmation dialog
-    var confirmation = confirm('Are you sure you want to delete this user?');
-
-    // Check and make sure the user confirmed
-    if (confirmation === true) {
-
-        // If they did, do our delete
-        $.ajax({
-            type: 'DELETE',
-            url: '/users/deleteuser/' + $(this).attr('rel')
-        }).done(function( response ) {
-
-            // Check for a successful (blank) response
-            if (response.msg === '') {
-            }
-            else {
-                alert('Error: ' + response.msg);
-            }
-
-            // Update the table
-            populateTable();
-
-        });
-
-    }
-    else {
-
-        // If they said no to the confirm, do nothing
-        return false;
-
-    }
+    $('#paymentInfoID').text(thisPaymentObject._id);
+    $('#paymentInfoMatching').text(thisPaymentObject.matching);
+    $('#paymentInfoWorking').text(thisPaymentObject.working);
+    $('#paymentInfoName').text(thisPaymentObject.name);
+    $('#paymentInfoType').text(thisPaymentObject.type);
+    $('#paymentInfoCard').text(thisPaymentObject.card);
+    $('#paymentInfoCondition').text(thisPaymentObject.condition);
+    $('#paymentInfoDate').text(thisPaymentObject.date);
+    $('#paymentInfoPrice').text(thisPaymentObject.price);
+    $('#paymentInfoTax').text(thisPaymentObject.tax);
+    $('#paymentInfoPaid').text(thisPaymentObject.paid);    
 
 };
