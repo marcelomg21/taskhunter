264eceb508f745d5462ffd7cdb45e7596b1769fb
marcelomg21 <marcelomg21@gmail.com>
2017-12-29 10:41:03 -0200

    Create payment.js

---
+++ routes/payment.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    /*var db = req.db;
+    var collection = db.collection('userlist');
+    collection.find({},function(e,docs){
+        res.json(docs);
+    });*/
+    
+    var db = req.db;
+    var query = {};    
+
+    db.collection('userlist').find(query).toArray(function (err, docs) {                                   
+            return res.json(docs);                
+    });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.collection('userlist');    
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.collection('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+module.exports = router;
