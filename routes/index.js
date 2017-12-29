95344795ceb0d9e2175a93c432f9f6ad2125637d
marcelomg21 <marcelomg21@gmail.com>
2017-12-29 12:23:00 -0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 router.get('/payments', function(req, res) {
     res.render('payment', {
         'pathToAssets': '/bootstrap-3.3.1',
-        'pathToSelectedTemplateWithinBootstrap' : '/bootstrap-3.3.1/docs/examples/carousel'
+        'pathToSelectedTemplateWithinBootstrap' : '/bootstrap-3.3.1/docs/examples/starter-template'
     });
 });
 
