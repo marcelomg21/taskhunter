dfec6a174bd68fe4b922eaab474b1de20cc5c23d
marcelomg21 <marcelomg21@gmail.com>
2017-12-29 11:14:30 -0200

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -4,17 +4,11 @@ var router = express.Router();
 /*
  * GET userlist.
  */
-router.get('/paymentlist', function(req, res) {
-    /*var db = req.db;
-    var collection = db.collection('userlist');
-    collection.find({},function(e,docs){
-        res.json(docs);
-    });*/
-    
+router.get('/paymentlist', function(req, res) {    
     var db = req.db;
     var query = {};    
 
-    db.collection('userlist').find(query).toArray(function (err, docs) {                                   
+    db.collection('payment_preferences').find(query).toArray(function (err, docs) {                                   
             return res.json(docs);                
     });
 });
