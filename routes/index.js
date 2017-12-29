5ba610b678c398cef9b930ba98880de756a277bd
marcelomg21 <marcelomg21@gmail.com>
2017-12-29 10:46:41 -0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/payment', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/payments', function(req, res) {
+  res.render('payment', { title: 'Pagamentos' });
 });
 
 router.get('/', function(req, res) {
