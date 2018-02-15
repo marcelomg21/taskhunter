a7161b5ef16c7e9f1e6fcf8b2ede79fa32540183
marcelomg21 <marcelomg21@gmail.com>
2018-02-15 13:01:02 -0200

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -1,12 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-/*
- * GET userlist.
- */
 router.get('/paymentlist', function(req, res) {    
     var db = req.db;
     var query = {};    
+
+    db.collection('payment_preferences').find(query).toArray(function (err, docs) {                                   
+            return res.json(docs);                
+    });
+});
+
+router.get('/paidPaymentlist', function(req, res) {    
+    var db = req.db;
+    var query = {paid:true};    
+
+    db.collection('payment_preferences').find(query).toArray(function (err, docs) {                                   
+            return res.json(docs);                
+    });
+});
+
+router.get('/notPaidPaymentlist', function(req, res) {    
+    var db = req.db;
+    var query = {paid:false};    
+
+    db.collection('payment_preferences').find(query).toArray(function (err, docs) {                                   
+            return res.json(docs);                
+    });
+});
+
+router.get('/transferedPaymentlist', function(req, res) {    
+    var db = req.db;
+    var query = {transfered:true};    
+
+    db.collection('payment_preferences').find(query).toArray(function (err, docs) {                                   
+            return res.json(docs);                
+    });
+});
+
+router.get('/notTransferedPaymentlist', function(req, res) {    
+    var db = req.db;
+    var query = {transfered:false};    
 
     db.collection('payment_preferences').find(query).toArray(function (err, docs) {                                   
             return res.json(docs);                
