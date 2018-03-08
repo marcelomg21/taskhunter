bf22bcaf480af799a5420993f803bba1a0a53c92
marcelomg21 <marcelomg21@gmail.com>
2018-03-08 20:40:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2059,14 +2059,14 @@ app.get('/api/users/:user_id/crossings', function (req, res) {
 
 				}
 
-				console.log('timeline_matching_crossings.services: ' + timeline_matching_crossings.services.toString());
-				console.log('timeline_working_crossings.services: ' + timeline_working_crossings.services.toString());
+				//console.log('timeline_matching_crossings.services: ' + timeline_matching_crossings.services.toString());
+				//console.log('timeline_working_crossings.services: ' + timeline_working_crossings.services.toString());
 				    
-				db.collection('service_preferences').findAndModify({
-					query: { user_id: parseInt(req.params.user_id) },
-					update: { user_id: parseInt(req.params.user_id), matching : { services: timeline_matching_crossings.services },  working : {services: timeline_working_crossings.services } },
-					upsert: true
-				});
+				db.collection('service_preferences').findAndModify(
+					{ user_id : parseInt(req.params.user_id) },
+					{ user_id : parseInt(req.params.user_id), matching : { services: timeline_matching_crossings.services },  working : {services: timeline_working_crossings.services } },
+					{ upsert : true}
+				);
 
 				/*db.collection('service_preferences').update({ 
 				    user_id: parseInt(req.params.user_id)},                                    
