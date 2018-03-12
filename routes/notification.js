9aee0894b4c04f7d7e32a684ee65db52ea87f922
marcelomg21 <marcelomg21@gmail.com>
2018-03-12 10:54:52 -0300

    Update notification.js

--- routes/notification.js
+++ routes/notification.js
@@ -1,25 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/*
- * GET userlist.
- */
-router.get('/userlist', function(req, res) {
-    /*var db = req.db;
-    var collection = db.collection('userlist');
-    collection.find({},function(e,docs){
-        res.json(docs);
-    });*/
-    
+router.get('/notificationlist/:user_id', function(req, res) {
     var db = req.db;
-    var query = {};    
+    var query = { user_id : parseInt(req.params.user_id) };
 
-    db.collection('users').find(query).toArray(function (err, docs) {                                   
+    db.collection('notification_preferences').find(query).toArray(function (err, docs) {                                   
             return res.json(docs);                
     });
 });
 
-router.post('/adduser', function(req, res) {
+router.get('/notificationalllist', function(req, res) {
+    var db = req.db;
+    var query = {};    
+
+    db.collection('notification_all_preferences').find(query).toArray(function (err, docs) {                                   
+            return res.json(docs);                
+    });
+});
+
+router.post('/addnotification', function(req, res) {
     var db = req.db;
     var collection = db.collection('userlist');    
     collection.insert(req.body, function(err, result){
@@ -29,7 +29,17 @@ router.post('/adduser', function(req, res) {
     });
 });
 
-router.get('/detailUser/:id', function(req, res) {    
+router.post('/addnotificationall', function(req, res) {
+    var db = req.db;
+    var collection = db.collection('userlist');    
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+router.get('/detailnotification/:id', function(req, res) {    
     var db = req.db;
     var ObjectId = require('mongodb').ObjectID;
     var userObjectId = ObjectId(req.params.id);
@@ -39,7 +49,17 @@ router.get('/detailUser/:id', function(req, res) {
 	});
 });
 
-router.post('/updateUser/:id', function(req, res) {
+router.get('/detailnotificationall/:id', function(req, res) {    
+    var db = req.db;
+    var ObjectId = require('mongodb').ObjectID;
+    var userObjectId = ObjectId(req.params.id);
+	
+    db.collection('users').find({'_id' : userObjectId}).toArray(function (err, docs_users) {
+		return res.json(docs_users);
+	});
+});
+
+router.post('/updatenotification/:id', function(req, res) {
     var db = req.db;
     var ObjectId = require('mongodb').ObjectID;
     var userObjectId = ObjectId(req.params.id);
@@ -55,13 +75,20 @@ router.post('/updateUser/:id', function(req, res) {
 	   });
 });
 
-router.delete('/deleteuser/:id', function(req, res) {
+router.post('/updatenotificationall/:id', function(req, res) {
     var db = req.db;
-    var collection = db.collection('userlist');
-    var userToDelete = req.params.id;
-    collection.remove({ '_id' : userToDelete }, function(err) {
-        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
-    });
+    var ObjectId = require('mongodb').ObjectID;
+    var userObjectId = ObjectId(req.params.id);
+	
+    db.collection('users').update(
+	   {_id : userObjectId}, 
+	   {$set: {discount_rate : req.body.discount_rate}}, 
+	   {upsert:false},
+	   function (err, result) {
+	      res.send(
+		  (err === null) ? { msg: '' } : { msg: err }
+	      );
+	   });
 });
 
 module.exports = router;
