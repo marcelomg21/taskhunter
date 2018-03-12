7a81742177c4c2cae92f6b3a48c4a9ffd4d3faa6
marcelomg21 <marcelomg21@gmail.com>
2018-03-12 16:10:45 -0300

    Update notification-all.js

--- public/javascripts/notification-all.js
+++ public/javascripts/notification-all.js
@@ -1,4 +1,4 @@
-var userListData = [];
+var notificationAllListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
@@ -7,7 +7,7 @@ $(document).ready(function() {
     populateTable();
 
     // Username link click
-    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+    $('#notificationAllList table tbody').on('click', 'td a.linkshownotificationall', showNotificationAllInfo);
     
     $('#goHome').on('click', goHome);
 
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
 function goHome(event) {
     event.preventDefault();
-    $(location).attr('href','http://nodejs-mongo-persistent-marcelomg21.1d35.starter-us-east-1.openshiftapps.com/users');
+    $(location).attr('href','http://nodejs-mongo-persistent-marcelomg21.1d35.starter-us-east-1.openshiftapps.com/notifications-all');
 };
 
 // Fill table with data
@@ -25,31 +25,31 @@ function populateTable() {
     var tableContent = '';
 
     // jQuery AJAX call for JSON
-    $.getJSON( '/user/userlist', function( data ) {
+    $.getJSON( '/notification-all/notificationalllist', function( data ) {
 
         // Stick our user data array into a userlist variable in the global object
-        userListData = data;
+        notificationAllListData = data;
 
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this._id + '" title="Mostrar Detalhes">' + this._id + '</a></td>';
+            tableContent += '<td><a href="#" class="linkshownotificationall" rel="' + this._id + '" title="Mostrar Detalhes">' + this._id + '</a></td>';
             tableContent += '<td>' + this.user_id + '</td>';
-            tableContent += '<td>' + this.full_user_name + '</td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td>' + this.gender + '</td>';
-            tableContent += '<td>' + this.city + '</td>';
+            tableContent += '<td>' + this.timestamp + '</td>';
+            tableContent += '<td>' + this.is_notified + '</td>';
+            tableContent += '<td>' + this.message_title + '</td>';
+            tableContent += '<td>' + this.message_data + '</td>';
             //tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
         });
 
         // Inject the whole content string into our existing HTML table
-        $('#userList table tbody').html(tableContent);
+        $('#notificationAllList table tbody').html(tableContent);
     });
 };
 
 // Show User Info
-function showUserInfo(event) {
+function showNotificationAllInfo(event) {
 
     // Prevent Link from Firing
     event.preventDefault();
@@ -57,31 +57,6 @@ function showUserInfo(event) {
     // Retrieve username from link rel attribute
     var thisID = $(this).attr('rel');
     
-    $(location).attr('href','http://nodejs-mongo-persistent-marcelomg21.1d35.starter-us-east-1.openshiftapps.com/detail-user/' + thisID);
-
-    // Get Index of object based on id value
-    /*var arrayPosition = userListData.map(function(arrayItem) { return arrayItem._id; }).indexOf(thisID);
-    var thisUserObject = userListData[arrayPosition];
-    //Populate Info Box
-    $('#userInfoID').text(thisUserObject._id);
-    $('#userInfoUserId').text(thisUserObject.user_id);
-    $('#userInfoFullUserName').text(thisUserObject.full_user_name);
-    $('#userInfoGender').text(thisUserObject.gender);
-    $('#userInfoCpf').text(thisUserObject.cpf);
-    $('#userInfoCellPhone').text(thisUserObject.cell_phone);
-    $('#userInfoBirthDate').text(thisUserObject.birth_date);
-    $('#userInfoEmail').text(thisUserObject.email);
-    $('#userInfoRegisterDate').text(thisUserObject.register_date);
-    $('#userInfoCountry').text(thisUserObject.country);
-    $('#userInfoCity').text(thisUserObject.city);
-    $('#userInfoNeighborhood').text(thisUserObject.neighborhood);
-    $('#userInfoState').text(thisUserObject.state);
-    $('#userInfoStreetAddress').text(thisUserObject.street_address);
-    $('#userInfoStreetNumber').text(thisUserObject.street_number);
-    $('#userInfoZipCode').text(thisUserObject.zip_code);
-    $('#userInfoBank').text(thisUserObject.bank);
-    $('#userInfoAgency').text(thisUserObject.agency);
-    $('#userInfoAccount').text(thisUserObject.account);
-    $('#userInfoDigit').text(thisUserObject.digit);*/
+    $(location).attr('href','http://nodejs-mongo-persistent-marcelomg21.1d35.starter-us-east-1.openshiftapps.com/detail-notification-all/' + thisID);
 
 };
