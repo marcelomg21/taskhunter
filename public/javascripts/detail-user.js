a4c14458f2cd662cf278f564b20db85623c79748
marcelomg21 <marcelomg21@gmail.com>
2018-06-04 22:30:22 -0300

    Update detail-user.js

--- public/javascripts/detail-user.js
+++ public/javascripts/detail-user.js
@@ -60,6 +60,7 @@ function populateTable() {
             $('#userInfoAccount').text(this.account);
             $('#userInfoDigit').text(this.digit);
             $('#userInfoDiscountRate').val(this.discount_rate);
+            $('#userInfoBlocked').val(this.is_blocked);
         });
 
         // Inject the whole content string into our existing HTML table
@@ -79,7 +80,10 @@ function populateTable() {
 function updateUser(event) {
     event.preventDefault();
     
+        var isBlocked = $('#userInfoBlocked').is(':checked') ? true : false;
+    
         var updateUserBody = {
+            'is_blocked': isBlocked,
             'discount_rate': $('#userInfoDiscountRate').val()
         }
         
