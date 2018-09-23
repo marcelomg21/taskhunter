648f7ae3a17fb858f8036fb6baf1dbbfd5cffd40
marcelomg21 <marcelomg21@gmail.com>
2018-09-22 21:39:14 -0300

    Update detail-feedback.js

--- public/javascripts/detail-feedback.js
+++ public/javascripts/detail-feedback.js
@@ -7,8 +7,7 @@ $(document).ready(function() {
     // Populate the user table on initial page load
     populateTable();
 
-    // Username link click
-    $('#saveUser').on('click', updateUser);
+    $('#saveFeedback').on('click', updateFeedback);
     
     $('#goHome').on('click', goHome);
 
@@ -29,38 +28,22 @@ function populateTable() {
     // Prevent Link from Firing
     event.preventDefault();
     
-    var userId = $(location).attr('href').split('detail-user/')[1];
+    var feedbackId = $(location).attr('href').split('detail-feedback/')[1];
     
     // jQuery AJAX call for JSON
-    $.getJSON( '/user/detailUser/' + userId, function( data ) {
+    $.getJSON( '/user/detailFeedback/' + feedbackId, function( data ) {
 
         // Stick our user data array into a userlist variable in the global object
         //detailPaymentData = data;
 
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
-            $('#userInfoID').text(this._id);
-            $('#userInfoUserId').text(this.user_id);
-            $('#userInfoFullUserName').text(this.full_user_name);
-            $('#userInfoGender').text(this.gender);
-            $('#userInfoCpf').text(this.cpf);
-            $('#userInfoCellPhone').text(this.cell_phone);
-            $('#userInfoBirthDate').text(this.birth_date);
-            $('#userInfoEmail').text(this.email);
-            $('#userInfoRegisterDate').text(this.register_date);
-            $('#userInfoCountry').text(this.country);
-            $('#userInfoCity').text(this.city);
-            $('#userInfoNeighborhood').text(this.neighborhood);
-            $('#userInfoState').text(this.state);
-            $('#userInfoStreetAddress').text(this.street_address);
-            $('#userInfoStreetNumber').text(this.street_number);
-            $('#userInfoZipCode').text(this.zip_code);
-            $('#userInfoBank').text(this.bank);
-            $('#userInfoAgency').text(this.agency);
-            $('#userInfoAccount').text(this.account);
-            $('#userInfoDigit').text(this.digit);
-            $('#userInfoDiscountRate').val(this.discount_rate);
-            $('#userInfoBlocked').attr('checked', this.is_blocked);
+            $('#feedbackInfoID').text(this._id);
+            $('#feedbackInfoMatchingId').text(this.matching);
+            $('#feedbackInfoWorkingId').text(this.working);
+            $('#feedbackInfoComment').text(this.comment);
+            $('#feedbackInfoAvaliation').text(this.evaluation);
+            $('#feedbackInfoApproved').attr('checked', this.approved);
         });
 
         // Inject the whole content string into our existing HTML table
@@ -77,28 +60,27 @@ function populateTable() {
     });
 };
 
-function updateUser(event) {
+function updateFeedback(event) {
     event.preventDefault();
     
-        var isBlocked = $('#userInfoBlocked').is(':checked') ? true : false;
+        var isApproved = $('#feedbackInfoApproved').is(':checked') ? true : false;
     
-        var updateUserBody = {
-            'is_blocked': isBlocked,
-            'discount_rate': $('#userInfoDiscountRate').val()
+        var updateFeedbackBody = {
+            'is_approved': isApproved
         }
         
-        var userId = $('#userInfoID').text();
+        var feedbackId = $('#feedbackInfoID').text();
 
         // Use AJAX to post the object to our adduser service
         $.ajax({
             type: 'POST',
-            data: updateUserBody,
-            url: '/user/updateUser/' + userId,
+            data: updateFeedbackBody,
+            url: '/user/updateFeedback/' + feedbackId,
             dataType: 'JSON'
         }).done(function( response ) {
             // Check for successful (blank) response
             if (response.msg === '') {
-                alert('Usuário salvo com sucesso.');
+                alert('Feedback salvo com sucesso.');
             }
             else {
                 alert('Erro ao salvar: ' + response.msg);
@@ -108,5 +90,5 @@ function updateUser(event) {
 
 function goHome(event) {
     event.preventDefault();
-    $(location).attr('href','http://ec2-18-228-85-34.sa-east-1.compute.amazonaws.com/users');
+    $(location).attr('href','http://ec2-18-228-85-34.sa-east-1.compute.amazonaws.com/feedbacks');
 };
