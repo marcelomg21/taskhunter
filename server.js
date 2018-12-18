65a7d78879ef0507325105a2a7d146654263a6cb
marcelomg21 <marcelomg21@gmail.com>
2018-12-18 19:14:48 -0200

    Update server.js

--- server.js
+++ server.js
@@ -180,7 +180,8 @@ var crossingNotificationsJob = new cronJob('0 0 */1 * * *', function(){
 				    console.log("Error sending notification pro:", error);
 			      });
 		      }
-		 }
+		   }
+		}
 	    }
 	}
 
